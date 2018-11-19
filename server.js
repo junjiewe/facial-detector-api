849108ddf4bf3bff583851537c9431208a270666
Junjie Wei <junjiewe@buffalo.edu>
2018-11-18 19:16:14 -0500

    adding port

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("app is running fine on 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running fine on ${process.env.PORT}`);
 });
